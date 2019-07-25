bdf45e6ba840bd8847317f73be5fee16d70c68de
Sulaiman Abolaji Bisiriyu <abolajibisiriyu@gmail.com>
2019-07-25 02:24:15 +0100

    makde lil changes to header button

--- src/blocks/HomeHeader/Link.js
+++ src/blocks/HomeHeader/Link.js
@@ -2,17 +2,36 @@ import styled from "styled-components";
 
 export default styled.a`
   width: 325px;
-  min-height: 104px;
+  /* min-height: 104px; */
+  min-height: 90px;
 
   background: #2d9cdb;
   border-radius: 50px;
 
   font-size: 1.2em;
-  padding: 15px 15px 5px;
+  padding: 15px 15px 0px;
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
   cursor: pointer;
+
+  & > span {
+  }
+
+  animation: pulse 2s infinite;
+  animation-timing-function: ease-in-out;
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
 `;
